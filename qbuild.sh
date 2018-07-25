4a85801c906645ef93d5d2e1a3967b6212a18cf9
Fugi <fugiman47@gmail.com>
2018-07-24 20:04:36 -0700

    update script

--- qbuild.sh
+++ qbuild.sh
@@ -1,3 +1,3 @@
-export DOCKER_HOST=192.168.99.101:2376
+export DOCKER_HOST=192.168.99.100:2376
 export GOPATH=/c/Users/fugi_000/go
 xgo --targets=windows/amd64 -ldflags="-H windowsgui" -out deckmaster -dest /c/Users/fugi_000/go/src/github.com/fugiman/deckmaster/dist/netlify ./client
